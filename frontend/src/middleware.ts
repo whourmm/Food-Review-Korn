36c225a3c14190925a3e23421b2160438d27e294
varinthorn <andavarinthorn1409@gmail.com>
2026-01-16 18:09:54 +0700

    fix: middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,14 +1,12 @@
-import { getToken } from "next-auth/jwt";
+"use client"
+import { useSession } from "next-auth/react";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({
-    req,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
+  const {data : session} = useSession();
 
-  if (!token) {
+  if (!session) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
