85eed38a4a895487282b8f19e39eba9405c0c5b4
RichyYhcir <pathomphu@gmail.com>
2026-02-07 11:28:59 +0800

    fix: add corsmiddleware

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
